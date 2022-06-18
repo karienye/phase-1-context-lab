22b6d6ee0bd287ce48101dc6f541bbeb79f8f285
leah karienye <karienyeleah@gmail.com>
2022-06-18 20:49:33 +0300

    commit tree d52654332c350634c019c272ff94b52fd6c0d1e9

--- index.js
+++ index.js
@@ -1,50 +1,76 @@
 /* Your Code Here */
-const  createEmployeeRecord = function (names, digits){ 
-       const newEmployee = [ firstName = names,
-        familyName= names,
-        titleField = names,
-        payPerhour= digits,
-         field = names,
-     timeInEvents = [],
-    timeOutEvents = [] ]
+let  createEmployeeRecord = function (names, digits){ 
+       return { 
+           firstName:names[0],
+        familyName: names[1],
+        titleField: names[2],
+        payPerhour: digits[3],
+     timeInEvents: [],
+    timeOutEvents: [] }
     
     }
-const createEmployeeRecords = function (array){
-    let newArray ; 
-    for (i= 0; i<newEmployee.length; i++){
-return newArray;
-}
-}
 
-const createTimeInEvent = function (type, hour, date){
-
+const createEmployeeRecords = function (recordsEmployee){
+    
+return recordsEmployee.map(function(names){
+    return createEmployeeRecord(names);
+});
 
 }
 
-const createTimeOutEvent = function (type, hour, date){
+const createTimeInEvent = function (timeIn){
+    let [date, hour] = timeIn.split(' ')
+
+    return timeInEvents.push ({
+        type: 'arrival',
+        hour: parseInt(hour),
+        date:'',
+    })
+  return this;
+}
+const createTimeOutEvent = function (timeOut){
+    let [date, hour] = timeOut.split(' ')
+
+    return timeOutEvents.push ({
+        type: 'arrival',
+        hour: parseInt(hour),
+        date:'',
+    })
+  return this;
 
 }
 
-const hoursWorkedOnDate = function (){
+const hoursWorkedOnDate = function (dateIn){
+    let workingHours = this.timeInEvents.find(function(e){
+        return e.date === dateIn;
+    })
+    let notWorkingHours = this.timeoutEvents.find(function(e){
+        return e.date === dateIn;
+    })
+    return (notWorkingHours.hour - workingHours.hour) / 100
 
 }
 
-const wagesEarnedOnDate = function (){
+const wagesEarnedOnDate = function (pay){ 
+    let wages = hoursWorkedOnDate.call(this, pay)
+     this.payPerhour 
+     return parseFloat(wages.toString())
 
 }
 
 const findEmployeeByFirstName = function (srcArray, firstName){
-
+    return srcArray.find(function(rec){
+        return rec.firstName === firstName
+    })
 }
 
 const calculatePayroll = function (createEmployeeRecords){
-
+    return createEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    })
 }
    // newEmployee.forEach(element => console.log(element));
 
-/*let filledArray = new Array(10)
-    .fill(null)
-    .map(()=> ({'hello':'goodbye'}))*/ 
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
